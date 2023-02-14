3e7b6db256941d83c5397c3aa3ce99996fef4be0
Henry Rosario <henryrosario056@gmail.com>
2023-02-13 22:42:46 -0400

    Creando Migracion y base de datos

--- Models/Libros.cs
+++ Models/Libros.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Libros
 {
+    [Key]
     public int LibroId { get; set; }
     
+    [Required(ErrorMessage ="Requiere el titulo")]
     public string? Titulo { get; set; }
-
+     [Required(ErrorMessage ="Requiere ingresar el precio")]
     public double  Precio { get; set; }
 
 }
